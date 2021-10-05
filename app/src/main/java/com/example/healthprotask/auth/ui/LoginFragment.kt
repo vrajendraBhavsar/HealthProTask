aeb19436bed8b6abc316018907142df9883049e0
Vrajendraa <Vrajendraa>
2021-10-05 20:02:50 +0530

    - removed unnecessary & commented code - implemented Fragment result api

--- app/src/main/java/com/example/healthprotask/auth/ui/LoginFragment.kt
+++ app/src/main/java/com/example/healthprotask/auth/ui/LoginFragment.kt
@@ -3,25 +3,40 @@ package com.example.healthproclienttask.auth.ui
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.example.healthprotask.R
 import com.example.healthprotask.auth.ui.WebViewFragment
 import com.example.healthprotask.databinding.FragmentLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
+
 @AndroidEntryPoint
 class LoginFragment : Fragment() {
     lateinit var binding: FragmentLoginBinding
     val TAG = "HEALTHPROTEST"
 
+    val authViewModel by viewModels<AuthViewModel>() //vm
+
     companion object{
         fun newInstance() = LoginFragment()
+        val requestKey = UUID.randomUUID().toString()   //request key for fragment result listener
     }
 
-    val authViewModel by viewModels<AuthViewModel>() //vm
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        childFragmentManager.setFragmentResultListener(requestKey, this){ _, bundle ->
+            val code = bundle
+            Log.d(TAG, "onViewCreated: bundle code: $code")
+            Toast.makeText(requireContext(), "code : $code", Toast.LENGTH_LONG).show()
+        }
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
@@ -31,41 +46,22 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentLoginBinding.bind(view)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
 
         binding.btnAuth.setOnClickListener {
-    //2
-//            getUrlFromIntent()
-    //1
-//            val clientToken = authViewModel.authorizationRequest()
-//            if (clientToken != ""){
-//                Toast.makeText(requireContext(), clientToken, Toast.LENGTH_SHORT).show()
-//                Log.d(TAG, "onActivityCreated: $clientToken")
-//            }else{
-//                Toast.makeText(requireContext(), "Couldn't get token..", Toast.LENGTH_SHORT).show()
-//            }
-     //3
+            val webViewFragment = WebViewFragment.newInstance()
+            val bundle = Bundle()
+            bundle.putString(WebViewFragment.REQUEST_KEY, requestKey) // bundle ma key set tari
+            webViewFragment.arguments = bundle
+
             requireActivity().supportFragmentManager.beginTransaction()
-                .replace(R.id.container, WebViewFragment.newInstance())
+                .replace(R.id.container, webViewFragment)
                 .addToBackStack("WebViewFragment")
                 .commit()
         }
     }
-    fun getUrlFromIntent() {
-        val url: String = "https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=23BKYF&redirect_uri=https%3A%2F%2Ffinished&scope=activity"
-        val intent: Intent = Intent(Intent.ACTION_VIEW)
-        intent.data = Uri.parse(url)
-        startActivity(intent)
-    }
 
-//    fun onBackPressed() {
-//        if (requireFragmentManager().backStackEntryCount > 0) {
-//            requireFragmentManager().popBackStack()
-//        } else {
-//            super.onBackPressed()
-//        }
-//    }
+    override fun onDestroy() {
+        super.onDestroy()
+        childFragmentManager.clearFragmentResultListener(requestKey) // cleared listener
+    }
 }
--- app/src/main/java/com/example/healthprotask/auth/ui/WebViewFragment.kt
+++ app/src/main/java/com/example/healthprotask/auth/ui/WebViewFragment.kt
@@ -4,18 +4,16 @@ import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
-import android.os.Handler
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.JavascriptInterface
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResult
 import com.example.healthproclienttask.auth.ui.LoginFragment
-import com.example.healthprotask.R
 import com.example.healthprotask.databinding.FragmentWebVeiwBinding
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
@@ -24,24 +22,27 @@ import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStream
 import java.io.InputStreamReader
-import java.net.URL
-import java.net.URLConnection
-import java.util.*
 
 class WebViewFragment : Fragment() {
     lateinit var binding: FragmentWebVeiwBinding
     private val TAG = "HEALTHPROTEST"
-    private val url = "https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=23BKYF&redirect_uri=https%3A%2F%2Fwww.mindinventory.com%2F&scope=activity"
-//    private var htmlContent = ""
+    private val url =
+        "https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=23BKYF&redirect_uri=https%3A%2F%2Fwww.mindinventory.com%2F&scope=activity"
 
     var redirect: Boolean = false
     var completelyLoaded: Boolean = true   //when page is loaded completely ..it will be true
 
-    companion object{
+    companion object {
         fun newInstance() = WebViewFragment()
+        const val REQUEST_KEY = "request_key"
+        const val DATA_KEY = "data_key"
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         binding = FragmentWebVeiwBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
         return binding.root
@@ -50,11 +51,21 @@ class WebViewFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val requestKey = arguments?.getString(REQUEST_KEY, null)
+        Log.d(TAG, "onViewCreated: requestKey: $requestKey")
+        if (LoginFragment.requestKey.isNullOrEmpty()) {
+//            GlobalScope.launch {
+//                            Thread.sleep(3000)
+//                            binding.progressbar.visibility = View.GONE
+//                        }
+//            fragmentManager?.beginTransaction()?.remove(this@WebViewFragment)?.commit()
+            return
+        }
         //....
-        binding.wbWebView.webViewClient = object : WebViewClient(){
+        binding.wbWebView.webViewClient = object : WebViewClient() {
             @SuppressLint("SetJavaScriptEnabled")
             override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-                if (!completelyLoaded){
+                if (!completelyLoaded) {
                     redirect = true
                 }
                 completelyLoaded = false
@@ -63,27 +74,6 @@ class WebViewFragment : Fragment() {
                 view?.apply {
                     url?.let { loadUrl(it) }
                     settings.javaScriptEnabled = true
-
-//                    addJavascriptInterface(object : Any() {
-//                        @JavascriptInterface
-//                        fun saveMetaTag(name: String?, value: String?) {
-//                            //Save meta tag value in some dictionary
-//                        }
-//
-//                        @JavascriptInterface
-//                        fun onNoMoreMetaTags() {
-//                            //Process meta tags
-//                        }
-//                    }, "metaLoader")
-
-//                    loadUrl("javascript:var metaTags = document.getElementsByTagName('meta'); if (metaTags != null && metaTags.length > 0) {var i; for (i=0;i<metaTags.length;i++) { window.metaLoader.saveMetaTag(metaTags[i].name, metaTags[i].content);  };  }; window.metaLoader.onNoMoreMetaTags();");
-
-                    //...
-//                    val aURL = URL(url)
-//                    val conn: URLConnection = aURL.openConnection()
-//                    conn.connect()
-//                    val `is`: InputStream = conn.getInputStream()
-//                    htmlContent = convertToString(`is`)
                 }
                 Log.d(TAG, "shouldOverrideUrlLoading1: $url")
 //                Log.d(TAG, "htmlContent: $htmlContent")
@@ -91,8 +81,11 @@ class WebViewFragment : Fragment() {
             }
 
             @SuppressLint("SetJavaScriptEnabled")
-            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
-                if (!completelyLoaded){
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                if (!completelyLoaded) {
                     redirect = true
                 }
                 completelyLoaded = false
@@ -103,15 +96,7 @@ class WebViewFragment : Fragment() {
                     settings.javaScriptEnabled = true
                 }
                 Log.d(TAG, "shouldOverrideUrlLoading2: ${request?.url}")
-//                Log.d(TAG, "htmlContent: $htmlContent")
-                 return super.shouldOverrideUrlLoading(view, request)
-
-//                return if(request?.url?.lastPathSegment == "error.html") {
-//                    view?.loadUrl(url)
-//                    true
-//                } else {
-//                    false
-//                }
+                return super.shouldOverrideUrlLoading(view, request)
             }
 
             fun convertToString(inputStream: InputStream?): String {
@@ -136,35 +121,39 @@ class WebViewFragment : Fragment() {
 
             override fun onPageFinished(view: WebView?, url: String?) {
                 super.onPageFinished(view, url)
-                if (!redirect){
+                if (!redirect) {
                     completelyLoaded = true
                 }
-                if (completelyLoaded && !redirect){ //means page is completely loaded
+                if (completelyLoaded && !redirect) { //means page is completely loaded
                     Log.d(TAG, "onPageFinished: $completelyLoaded")
-                    if (!url.isNullOrEmpty()){
+                    if (!url.isNullOrEmpty()) {
                         Log.d(TAG, "onPageFinished: url link : $url")
-//                        Handler().postDelayed({
-//                            binding.progressbar.visibility = View.GONE
-//                        }, 3000)
-
-//                        GlobalScope.launch {
-//                            delay(3000)
-//                            Thread.sleep(3000)
-//                            binding.progressbar.visibility = View.GONE
-//                        }
-//                        fragmentManager?.beginTransaction()?.remove(this@WebViewFragment)?.commit()
                         val uri: Uri = Uri.parse(url)
                         val code: String? = uri.getQueryParameter("code")
                         Log.d(TAG, "onPageFinished: code = $code")
+
+                        if (requestKey != null) {
+                            setFragmentResult(
+                                requestKey,
+                                Bundle().apply {
+                                    putString(DATA_KEY, code)
+                                }
+                            )
+//                            GlobalScope.launch {
+//                                Thread.sleep(3000)
+//                                binding.progressbar.visibility = View.GONE
+//                            }
+//                            fragmentManager?.beginTransaction()?.remove(this@WebViewFragment)?.commit()//back to parent fragment
+                        }
                     }
-                }
-                else{
+                } else {
                     redirect = false
                 }
             }
         }
         //Mention chrome latest version...
-        binding.wbWebView.settings.userAgentString = "Chrome/94.0.4606.71 Mobile"   //to avoid possible errors from occurring in latest versions
+        binding.wbWebView.settings.userAgentString =
+            "Chrome/94.0.4606.71 Mobile"   //to avoid possible errors from occurring in latest versions
         binding.wbWebView.loadUrl(url)
     }
 }
